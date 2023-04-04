31049198ed944ef64b77a2714115be0aa84b0cc1
lokendra-chaulagain <lokendrachaulagain803@gmail.com>
2023-04-04 22:09:56 +0545

    author

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,15 +5,24 @@ import NewsLetterModal from "../NewsLetterModal";
 
 const Footer = () => {
   return (
-    <div
-      className="px-3 px-sm-5 footer_print"
-      style={{ backgroundColor: "#f8f9fa" }}>
+    <div className="px-3 px-sm-5  bg-grey">
       <div className=" d-md-flex justify-content-between mt-4 pt-4 ">
         <NewsLetterModal />
         <FooterSmallDeviceSubscribe />
         <div className="my-md-5 d-flex justify-content-center custom-fs-11">&copy;2022 BROCADE</div>
         <FooterRightSide />
       </div>
+
+      <p className="small mt-4 mb-1 text-muted text-center fz-12">
+        Designed & Developed By&nbsp;
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://lokendra-portfolio.vercel.app">
+          Lokendra Chaulagain
+        </a>{" "}
+        No Right Reserved , Free To Use .
+      </p>
     </div>
   );
 };
