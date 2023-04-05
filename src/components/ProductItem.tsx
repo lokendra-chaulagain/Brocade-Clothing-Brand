574bdd96bda8816d20af697ae03ae7969b8d1985
lokendra-chaulagain <lokendrachaulagain803@gmail.com>
2023-04-05 17:14:59 +0545

    product name was missing

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -8,7 +8,9 @@ export default function ProductItem() {
     <div className="row d-flex">
       {products &&
         products.map((product: any, index: any) => (
-          <div key={index} className="col-12 col-sm-6 col-md-4 col-lg-3 card-group ">
+          <div
+            key={index}
+            className="col-12 col-sm-6 col-md-4 col-lg-3 card-group ">
             <div className=" card border-0 mb-4 hover_underline no_selection ">
               <Image
                 src={product.image}
@@ -20,9 +22,9 @@ export default function ProductItem() {
 
               <Link href={"/product/bomber-jacket-premium-original-brocade"}>
                 <div className="card-body text-center cursor-pointer ">
-                  <p className="h6 text-uppercase fw-bold ">Product Name</p>
+                  <p className="h6 text-uppercase fw-bold ">{product.name}</p>
                   <p className="h6">
-                    NPR. 500 &nbsp;<s>NPR 700</s>
+                    NPR. {product.price} &nbsp;<s>NPR.5000</s>
                   </p>
                 </div>
               </Link>
--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,30 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import Cart from "./Cart";
 import Link from "next/link";
 import RightMenu from "./RightMenu";
 import UserHead from "./UserHead";
+import { BsFillSunFill } from "react-icons/bs";
+import { IoMdMoon } from "react-icons/io";
 
 const NavBar = () => {
+  const [mode, setMode] = useState("dark");
+
+  const selectDarkMode = () => {
+    setMode("dark");
+  };
+
+  const selectLightMode = () => {
+    setMode("light");
+  };
+
+  console.log(mode)
   return (
-    <nav className="navbar bg-black position-fixed w-100 py-3 z_999">
+    <nav className="navbar bg-black position-fixed w-100 py-3 z_999 no_selection">
       <div className="container">
         <Link href="/">
           <p className="h2 text-white cursor-pointer">Brocade</p>
         </Link>
         <div className="d-flex gap-4 align-items-center ">
-          <Link passHref legacyBehavior href={"/custom"}>
+          <div>
+            {mode == "dark" && (
+              <BsFillSunFill
+                onClick={selectLightMode}
+                color="white"
+                size={20}
+                className="cursor-pointer"
+              />
+            )}
+            {mode == "light" && (
+              <IoMdMoon
+                onClick={selectDarkMode}
+                color="white"
+                size={20}
+                className="cursor-pointer"
+              />
+            )}
+          </div>
+
+          <Link
+            passHref
+            legacyBehavior
+            href={"/custom"}>
             <p className="h6 color_white cursor-pointer d-none d-sm-block">Customization</p>
           </Link>
 
-          <Link passHref legacyBehavior href={"/lunching"}>
+          <Link
+            passHref
+            legacyBehavior
+            href={"/lunching"}>
             <p className="h6 color_white cursor-pointer d-none d-sm-block">Booking</p>
           </Link>
 
-          <Link passHref legacyBehavior href={"/exclusive"}>
+          <Link
+            passHref
+            legacyBehavior
+            href={"/exclusive"}>
             <p className="h6 color_white cursor-pointer d-none d-sm-block">Exclusive</p>
           </Link>
 
-          <Link passHref legacyBehavior href={"/shop"}>
+          <Link
+            passHref
+            legacyBehavior
+            href={"/shop"}>
             <p className="h6 color_white cursor-pointer d-none d-sm-block">Shop</p>
           </Link>
 
